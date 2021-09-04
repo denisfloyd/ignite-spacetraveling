2046da7cf3e2992372be6916d19f21f9bd20c509
denismend <denisladeira1@gmail.com>
2021-09-04 15:50:52 -0300

    feat: navigation posts

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,17 +1,25 @@
 import { useEffect } from 'react';
 
-export default function Comments(): JSX.Element {
+const Comments = (): any => {
   useEffect(() => {
+    // document.getElementById;
     const script = document.createElement('script');
     const anchor = document.getElementById('inject-comments-for-uterances');
+
+    if (anchor.children.length > 0) {
+      anchor.removeChild(anchor.children[0]);
+    }
+
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
+    script.setAttribute('async', 'async');
     script.setAttribute('repo', 'denismend/ignite-spacetraveling');
     script.setAttribute('issue-term', 'pathname');
-    script.setAttribute('theme', 'github-dark');
-    script.setAttribute('async', 'async');
+    script.setAttribute('theme', 'dark-blue');
     anchor.appendChild(script);
-  }, []);
+  });
 
   return <div id="inject-comments-for-uterances" />;
-}
+};
+
+export default Comments;
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,12 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import Prismic from '@prismicio/client';
 import { RichText } from 'prismic-dom';
 
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
-import { format, parseISO } from 'date-fns';
+import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 
 import { useRouter } from 'next/router';
@@ -13,8 +14,13 @@ import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
-import Comments from '../../components/Comments';
 import PreviewButton from '../../components/PreviewButton';
+import Comments from '../../components/Comments';
+
+interface NavigationPosts {
+  title: string;
+  uid: string;
+}
 
 interface Post {
   uid: string;
@@ -39,11 +45,16 @@ interface Post {
 interface PostProps {
   post: Post;
   preview: boolean;
+  navigation: {
+    nextPost: NavigationPosts | null;
+    previousPost: NavigationPosts | null;
+  };
 }
 
 export default function Post({
   post: postFromProps,
   preview,
+  navigation: { nextPost, previousPost },
 }: PostProps): JSX.Element {
   const router = useRouter();
 
@@ -128,6 +139,30 @@ export default function Post({
           </div>
         </article>
 
+        <aside className={styles.footer}>
+          <div>
+            {previousPost && (
+              <>
+                <p>{previousPost.title}</p>
+                <Link href={`/post/${previousPost.uid}`}>
+                  <a>Post anterior</a>
+                </Link>
+              </>
+            )}
+          </div>
+
+          <div>
+            {nextPost && (
+              <>
+                <p>{nextPost.title}</p>
+                <Link href={`/post/${nextPost.uid}`}>
+                  <a>Próximo post</a>
+                </Link>
+              </>
+            )}
+          </div>
+        </aside>
+
         <Comments />
 
         <PreviewButton preview={preview} />
@@ -137,23 +172,8 @@ export default function Post({
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const prismic = getPrismicClient();
-
-  const posts = await prismic.query(
-    [Prismic.predicates.at('document.type', 'post')],
-    { pageSize: 3 }
-  );
-
-  const paths = posts.results.map(result => {
-    return {
-      params: {
-        slug: result.uid,
-      },
-    };
-  });
-
   return {
-    paths,
+    paths: [],
     fallback: 'blocking',
   };
 };
@@ -170,6 +190,26 @@ export const getStaticProps: GetStaticProps<PostProps> = async ({
   const response = await prismic.getByUID('post', String(slug), {
     ref: previewData?.ref || null,
   });
+
+  const previousPostResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title'],
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+    }
+  );
+
+  const nextPostResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title'],
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+    }
+  );
 
   const post: Post = {
     uid: response.uid,
@@ -188,6 +228,20 @@ export const getStaticProps: GetStaticProps<PostProps> = async ({
     props: {
       post,
       preview,
+      navigation: {
+        previousPost: previousPostResponse?.results[0]
+          ? {
+              title: previousPostResponse?.results[0].data.title,
+              uid: previousPostResponse?.results[0].uid,
+            }
+          : null,
+        nextPost: nextPostResponse?.results[0]
+          ? {
+              title: nextPostResponse?.results[0].data.title,
+              uid: nextPostResponse?.results[0].uid,
+            }
+          : null,
+      },
     },
     revalidate: 60 * 30, // 30 minutos
   };
